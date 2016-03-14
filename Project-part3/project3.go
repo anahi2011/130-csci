3b7a85f6eaac142aa6b16373d044a91c632472e3
Anahi Plasencia <anahiplasencia2010@hotmail.com>
2016-03-14 11:44:51 -0700

    PART 3

---
+++ Project-part3/project3.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+	"html/template"
+	"log"
+	"github.com/nu7hatch/gouuid"
+)
+func fat (res http.ResponseWriter,req, *http.Request){
+	tpl, err := template.ParseFiles("index.html")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	name := req.FormValue("name")
+	age := req.FormValue("age")
+
+	cookie, err := req.Cookie("user")
+	id, _ := uuid.NewV4()
+	cookie := &http.Cookie{
+		Name: "user",
+		Value: id.String() + name + age,
+		HttpOnly: true,
+	}
+	http.SetCookie(res, cookie)
+	tpl.Execute(res, nil)
+}
+
+func main() {
+	http.HandleFunc("/", upload)
+	http.ListenAndServe(":8040", nil)
+}
