698c8ef02051a761e583914ba283d36ba3750571
Anahi Plasencia <anahiplasencia2010@hotmail.com>
2016-04-04 10:29:04 -0700

    URL-name
    displays name in the url

---
+++ URL-localhost8080/main.go
@@ -0,0 +1,19 @@
+
+package main
+
+import (
+	"io"
+	"net/http"
+	"strings"
+)
+
+func handle(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/html; charset=utf-8")
+	nm := strings.Split(req.URL.Path, "/")
+	io.WriteString(res, nm[1])
+}
+
+func main() {
+
+	http.HandleFunc("/", handle)
+	http.ListenAndServe(":8080", nil)
